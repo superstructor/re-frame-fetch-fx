d0338a3da3004ecd3c13158f3bef169f7799a76f
Isaac Johnston <isaac@superstruct.nz>
2020-03-12 12:37:45 +1300

    Upgrade re-frame to 0.12.0

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
                                org.clojure/google-closure-library
                                org.clojure/google-closure-library-third-party]]
                  [thheller/shadow-cljs      "2.8.92"   :scope "provided"]
-                 [re-frame                  "0.10.9"   :scope "provided"]
+                 [re-frame                  "0.12.0"   :scope "provided"]
 
                  [cljs-bean                 "1.5.0"]]
 
