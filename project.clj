d657d2c7eb332c21cca5d0233c71f923aaf4902c
Isaac Johnston <isaac@superstruct.nz>
2021-04-11 14:38:37 +1200

    Upgrade binaryage/devtools to 1.0.3

--- project.clj
+++ project.clj
@@ -11,7 +11,7 @@
                  [thheller/shadow-cljs      "2.11.18"   :scope "provided"]
                  [re-frame                  "1.2.0"     :scope "provided"]]
 
-  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.2"]]}}
+  :profiles {:dev {:dependencies [[binaryage/devtools "1.0.3"]]}}
 
   :plugins      [[day8/lein-git-inject "0.0.14"]
                  [lein-shadow          "0.3.1"]
